5eb73634fbfbef91158ca079ab700ede603b2f95
Matteo Santandrea <mattsantand@gmail.com>
2019-11-07 00:29:20 +0100

    Add new branch

--- medianSorted.py
+++ medianSorted.py
@@ -13,31 +13,16 @@ def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
     sizeNum1 = len(nums1)
     sizeNum2 = len(nums2)
     even = (sizeNum1 + sizeNum2) % 2 == 0
-
+    c1 = 0
+    c2 = 0
+    found = False
     if even:
-        threshold = int((sizeNum1 + sizeNum2) / 2)
+        positionMedian = (sizeNum1 + sizeNum2) // 2 - 1
     else:
-        threshold = (sizeNum1 + sizeNum2) // 2 + 1
+        positionMedian = (sizeNum1 + sizeNum2) // 2
+    while not found:
+        if nums1[c1] > c2[]:
 
-    print(sizeNum1, sizeNum2, even, threshold)
-    iter = 0
-    while len(nums1) + len(nums2) > threshold:
-        print(iter)
-        print(nums1, nums2)
-        if nums1[0] < nums2[0]:
-            nums1.pop(0)
-        else:
-            nums2.pop(0)
-        print(nums1, nums2)
-        print("\n")
-        iter += 1
-
-    if not even:
-        median = min([nums1[0], nums2[0]])
-    else:
-        median = min([(nums1[0] + nums2[0]) / 2,
-                      (nums1[0] + nums1[1]) / 2,
-                      (nums2[0] + nums2[1]) / 2])
     return median
 
 
@@ -49,10 +34,11 @@ def ex1():
 
 
 def ex2():
-    a = [1, 1, 1, 1, 1, 1, 30]
-    b = [1, 1, 1, 1, 2, 10]
+    a = [1, 2]
+    b = [3, 4]
 
     assert findMedianSortedArrays(a, b) == 1
+
 
 if __name__ == '__main__':
     # ex1()
